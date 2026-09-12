e99b69905bffd7108cf92c2743040e98fc97018d
Naim Guenette Harvey <2360188@cegepmontpetit.ca>
2026-09-12 17:15:28 -0400

    FCT: Ajout du projet en base de données

---
+++ ProjetBD/Models/Utilisateur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace projetWeb.Models;
+
+[Table("Utilisateur", Schema = "Auth")]
+[Index("Courriel", Name = "UC_Utilisateur_Courriel", IsUnique = true)]
+public partial class Utilisateur
+{
+    [Key]
+    [Column("UtilisateurID")]
+    public int UtilisateurId { get; set; }
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string Courriel { get; set; } = null!;
+
+    [StringLength(255)]
+    [Unicode(false)]
+    public string MotDePasse { get; set; } = null!;
+}
